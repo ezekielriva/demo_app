06c28a83a29b27c5b638611af09ce6b24dcd63af
Ezekiel <ezerivadeneiral@gmail.com>
2013-12-03 14:22:42 -0300

    Adds about page rspec and code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DemoApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
   resources :microposts
 
   resources :users
@@ -45,7 +46,7 @@ DemoApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
