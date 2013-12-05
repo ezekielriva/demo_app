d888a68ca7443c263297ddb712876d5559527c14
Ezekiel <ezerivadeneiral@gmail.com>
2013-12-05 16:16:50 -0300

    Adds signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 DemoApp::Application.routes.draw do
+  get "users/new"
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
-  match '/help', to: 'static_pages#help', via: 'get'
-  match '/about', to: 'static_pages#about', via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',      to: 'users#new',                via: 'get'
+  match '/help',        to: 'static_pages#help',        via: 'get'
+  match '/about',       to: 'static_pages#about',       via: 'get'
+  match '/contact',     to: 'static_pages#contact',     via: 'get'
 
   resources :microposts
   resources :users
