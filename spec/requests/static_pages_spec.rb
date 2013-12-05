9b9317cb5944ce84e9352a5ea240bf5f02881de7
Ezekiel <ezerivadeneiral@gmail.com>
2013-12-05 16:43:00 -0300

    Adds spec for ApplicationHelper Adds spec for workflow page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,19 @@ describe "StaticPages" do
 
     it_should_behave_like "all static pages"
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    should have_title full_title 'About Us'
+    click_link "Help"
+    should have_title full_title 'Help'
+    click_link "Contact"
+    should have_title full_title 'Contact'
+    click_link "Home"
+    click_link "Sign up now!"
+    should have_title "Sign up"
+    click_link "sample app"
+    should have_title full_title ''
+  end
 end
